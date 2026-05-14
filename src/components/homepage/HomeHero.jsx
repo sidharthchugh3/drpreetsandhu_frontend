feff7c1163e0357a3fa605a2ba0b4a3411070aad
Divanshu Garg <divanshu@gigmediaapp.com>
2026-05-14 14:38:45 +0530

    format

--- src/components/homepage/HomeHero.jsx
+++ src/components/homepage/HomeHero.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const HomeHero = () => {
-  return <div>HomeHero</div>;
+    return <div>HomeHero</div>;
 };
 
 export default HomeHero;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import HomeHero from "../components/homepage/HomeHero";
+import React from 'react';
+import HomeHero from '../components/homepage/HomeHero';
 
 const Homepage = () => {
-  return <HomeHero />;
+    return <HomeHero />;
 };
 
 export default Homepage;
